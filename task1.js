5f7bdda2df803765103fa46ea800a4772516631e
Temetski33 <teemu.laasio@gmail.com>
2025-10-24 13:32:52 +0300

    Task 1 functional ver

--- task1.js
+++ task1.js
@@ -1,9 +1,9 @@
-// F = (C * 9/5) + 32, kelvn + 273.15
+// F = (C * 9/5) + 32, K = C + 273.15
 
 const degreeC = prompt('Input temperature (C) to convert: ');
 
 const degreeF = (degreeC * 9) / 5 + 32;
-const degreeK = degreeC + 273.15;
+const degreeK = degreeC * 1 + 273.15;
 
 const outputElement = document.querySelector('main');
 
