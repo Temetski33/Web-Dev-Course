fd80f0dd5d68719f1d4c3e121994444300af0a7b
Temetski33 <teemu.laasio@gmail.com>
2025-10-24 13:22:57 +0300

    Print fahrenheit to main

--- task1.js
+++ task1.js
@@ -1,1 +1,14 @@
 console.log('Hello all the worlds');
+
+// F = (C * 9/5) + 32, kelvn + 273.15
+
+const degreeC = prompt('Input temperature (C) to convert: ');
+console.log(degreeC);
+
+const degreeF = (degreeC * 9) / 5 + 32;
+const degreeK = degreeC + 273.15;
+
+const outputElement = document.querySelector('main');
+
+outputElement.innerText = `${degreeC} in Celsius is ${degreeF} in Fahrenheit.`;
+// outputElement.innerText = `${degreeC} in Celsius is ${degreeK} in Kelvin.`;
