a8ac9d7007911f0d52d3808c73ca009be38130f8
Temetski33 <teemu.laasio@gmail.com>
2025-10-24 14:07:14 +0300

    Task 2 functional version

--- task2.js
+++ task2.js
@@ -5,8 +5,8 @@ const y1 = prompt('Input Y coordinate of the first point: ');
 const x2 = prompt('Input X coordinate of the second point: ');
 const y2 = prompt('Input Y coordinate of the second point: ');
 
-const distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 
 const outputElement = document.querySelector('main');
 
-outputElement.innerText = distance;
+outputElement.innerText = `The distance between (${x1}, ${y1}) and (${x2}, ${y2}) is ${distance}.`;
