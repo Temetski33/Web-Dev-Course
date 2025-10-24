948bc72e7a2bc2c9ed86e064e529d7ce8302f09e
Temetski33 <teemu.laasio@gmail.com>
2025-10-24 13:26:57 +0300

    Add Kelvin print, remove console logs

--- task1.js
+++ task1.js
@@ -1,9 +1,6 @@
-console.log('Hello all the worlds');
-
 // F = (C * 9/5) + 32, kelvn + 273.15
 
 const degreeC = prompt('Input temperature (C) to convert: ');
-console.log(degreeC);
 
 const degreeF = (degreeC * 9) / 5 + 32;
 const degreeK = degreeC + 273.15;
@@ -11,4 +8,5 @@ const degreeK = degreeC + 273.15;
 const outputElement = document.querySelector('main');
 
 outputElement.innerText = `${degreeC} in Celsius is ${degreeF} in Fahrenheit.`;
-// outputElement.innerText = `${degreeC} in Celsius is ${degreeK} in Kelvin.`;
+outputElement.innerText += '\n';
+outputElement.innerText += `${degreeC} in Celsius is ${degreeK} in Kelvin.`;
