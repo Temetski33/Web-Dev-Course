f4853a5df1a12995b2d2c0fd4d7360b276d8e707
Temetski33 <teemu.laasio@gmail.com>
2025-10-24 13:59:22 +0300

    Fix equation

---
+++ task2.js
@@ -0,0 +1,12 @@
+// Distance = √((x2 - x1)^2 + (y2 - y1)^2)
+
+const x1 = prompt('Input X coordinate of the first point: ');
+const y1 = prompt('Input Y coordinate of the first point: ');
+const x2 = prompt('Input X coordinate of the second point: ');
+const y2 = prompt('Input Y coordinate of the second point: ');
+
+const distance = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+const outputElement = document.querySelector('main');
+
+outputElement.innerText = distance;
