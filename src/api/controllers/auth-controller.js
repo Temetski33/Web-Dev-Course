8b34191761a9e2ddbf312caaf9e0b47d874062eb
Temetski33 <teemu.laasio@gmail.com>
2025-11-07 13:25:56 +0200

    Add auth router and controller

--- src/api/controllers/auth-controller.js
+++ src/api/controllers/auth-controller.js
@@ -1,11 +1,11 @@
  import jwt from 'jsonwebtoken';
  import bcrypt from 'bcrypt';
- import {getUserByUsername} from '../models/user-model.js';
+ import {findUserByUsername} from '../models/user-model.js';
  import 'dotenv/config';
 
  const postLogin = async (req, res) => {
    console.log('postLogin', req.body);
-   const user = await getUserByUsername(req.body.username);
+   const user = await findUserByUsername(req.body.username);
    if (!user) {
      res.sendStatus(401);
      return;
---
+++ src/api/routes/auth-router.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import {
+  postLogin
+} from '../controllers/auth-controller.js';
+
+const authRouter = express.Router();
+
+authRouter.route('/login').post(postLogin);
+
+export default authRouter;
