d7cf2db708bcc1ff19add703499eec35674d4b47
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 19:35:09 +0200

    Change print into function

--- task6.js
+++ task6.js
@@ -6,17 +6,28 @@ iterations = Number(iterations);
 function addMovies() {
   for (let index = 1; index <= iterations; index++) {
     const input1 = prompt('Name: ');
-    const input2 = prompt('Rating: ');
+    let input2 = prompt('Rating: ');
+    input2 = Number(input2);
+
     const movie = {name: input1, rating: input2};
     movies.push(movie);
   }
 }
 
-addMovies();
+function printMovies() {
+  const outputElement = document.getElementById('output');
 
-const outputElement = document.getElementById('output');
+  movies.sort((a, b) => Number(b.rating) - Number(a.rating));
 
-for (const movie of movies) {
-  outputElement.innerText +=
-    'Movie: ' + movie.name + ' (rating: ' + movie.rating + ')\n';
+  outputElement.innerText = 'MOVIES LIST\n';
+
+  for (const movie of movies) {
+    outputElement.innerText +=
+      'Movie: ' + movie.name + ' (rating: ' + movie.rating + ')\n';
+  }
+
+  outputElement.innerText += 'Highest ratest movie: ' + movies[0].name;
 }
+
+addMovies();
+printMovies();
