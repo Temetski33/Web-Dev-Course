9b4cfcc817fd1d2ae7988c2ee6a1e6a1211604a6
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 19:20:29 +0200

    Add initial structure

--- task6.js
+++ task6.js
@@ -1,11 +1,22 @@
 let movies = [];
 
-const iterations = 3;
+let iterations = prompt('Enter the number of movies you want to rate: ');
+iterations = Number(iterations);
 
-for (let index = 1; index <= iterations; index++) {
-  const input1 = prompt('Name: ')
-  const input2 = prompt('Rating: ')
-  const movie = {name: input1, rating: input2}
-  movies.push(movie)
+function addMovies() {
+  for (let index = 1; index <= iterations; index++) {
+    const input1 = prompt('Name: ');
+    const input2 = prompt('Rating: ');
+    const movie = {name: input1, rating: input2};
+    movies.push(movie);
+  }
+}
 
+addMovies();
+
+const outputElement = document.getElementById('output');
+
+for (const movie of movies) {
+  outputElement.innerText +=
+    'Movie: ' + movie.name + ' (rating: ' + movie.rating + ')\n';
 }
