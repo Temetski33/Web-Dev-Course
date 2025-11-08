e99ba9458bc89103037d44b86aad382377d4470c
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 08:01:57 +0200

    Task 1 complete ver

--- task1.js
+++ task1.js
@@ -1,2 +1,16 @@
 const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
-console.log(fruits)
+console.log('Fruits: ', fruits);
+console.log('Length of fruits: ', fruits.length);
+console.log('Element at index 2: ', fruits[2]);
+console.log('Last Element of Fruits: ', fruits.at(-1));
+
+let vegetables = [];
+
+for (let index = 1; index <= 3; index++) {
+  let veggie = prompt('Input a vegetable: ');
+  vegetables.push(veggie)
+}
+
+console.log('Vegetables: ', vegetables);
+console.log('Length of vegetables: ', vegetables.length);
+
