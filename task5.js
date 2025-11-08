ef097e3978f30e8890ec9321c70e44f960fdf5c9
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 06:29:53 +0200

    Fix final print

--- task5.js
+++ task5.js
@@ -5,8 +5,8 @@ const outputElement = document.querySelector('main');
 
 if (Number.isNaN(integer)) {
   outputElement.innerText = `Wrong input! Refresh the page to try again.\n`;
-} else if (!Number.isInteger(integer)){
-    outputElement.innerText = `Do not input decimals. Refresh the page to try again.\n`;
+} else if (!Number.isInteger(integer)) {
+  outputElement.innerText = `Do not input decimals. Refresh the page to try again.\n`;
 } else {
   evaluateScore();
 }
@@ -19,8 +19,9 @@ function evaluateScore() {
     for (let i = 1; i <= integer; i++) {
       counter = counter + i;
     }
-    outputElement.innerText += `heihei ${counter}`;
+    outputElement.innerText += `The sum of all natural numbers up to your input is ${counter}`;
   } else {
-    outputElement.innerText += 'Your input was 0 or negative. Refresh the page and input a positive number.';
+    outputElement.innerText +=
+      'Your input was 0 or negative. Refresh the page and input a positive number.';
   }
 }
