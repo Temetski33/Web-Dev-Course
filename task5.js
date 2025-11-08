1c602886299c20781a07f0b4917a23a4d186cd47
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 06:27:04 +0200

    Add check for decimals

---
+++ task5.js
@@ -0,0 +1,26 @@
+let integer = prompt('Input a positive number: ');
+integer = Number(integer);
+
+const outputElement = document.querySelector('main');
+
+if (Number.isNaN(integer)) {
+  outputElement.innerText = `Wrong input! Refresh the page to try again.\n`;
+} else if (!Number.isInteger(integer)){
+    outputElement.innerText = `Do not input decimals. Refresh the page to try again.\n`;
+} else {
+  evaluateScore();
+}
+
+function evaluateScore() {
+  outputElement.innerText += `The input is ${integer}\n`;
+
+  if (integer > 0) {
+    let counter = 0;
+    for (let i = 1; i <= integer; i++) {
+      counter = counter + i;
+    }
+    outputElement.innerText += `heihei ${counter}`;
+  } else {
+    outputElement.innerText += 'Your input was 0 or negative. Refresh the page and input a positive number.';
+  }
+}
