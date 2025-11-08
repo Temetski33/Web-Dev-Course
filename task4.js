a81712cfcb6092bc9f33b85686d0bce2aede3095
Temetski33 <teemu.laasio@gmail.com>
2025-11-08 06:04:35 +0200

    Edit print

--- task4.js
+++ task4.js
@@ -10,20 +10,20 @@ if (Number.isNaN(score)) {
 }
 
 function evaluateScore() {
-  outputElement.innerText += `The input is ${score}.\n`;
+  outputElement.innerText += `The input is ${score}\n`;
 
   if (score >= 0 && score <= 39) {
-    outputElement.innerText += "The grade is 0";
+    outputElement.innerText += "Your grade is 0";
   } else if (score >= 40 && score <= 51) {
-    outputElement.innerText += "The grade is 1";
+    outputElement.innerText += "Your grade is 1";
   } else if (score >= 52 && score <= 63) {
-    outputElement.innerText += "The grade is 2";
+    outputElement.innerText += "Your grade is 2";
   } else if (score >= 64 && score <= 75) {
-    outputElement.innerText += "The grade is 3";
+    outputElement.innerText += "Your grade is 3";
   } else if (score >= 76 && score <= 87) {
-    outputElement.innerText += "The grade is 4";
+    outputElement.innerText += "Your grade is 4";
   } else if (score >= 88 && score <= 99) {
-    outputElement.innerText += "The grade is 5";
+    outputElement.innerText += "Your grade is 5";
   } else {
     outputElement.innerText += "Score is out of range. Refresh page to input again."
   }
