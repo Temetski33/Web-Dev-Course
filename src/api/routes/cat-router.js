a80f49fa433d3b2dc066d1cdef37bcc7372d3219
Temetski33 <teemu.laasio@gmail.com>
2025-11-14 11:20:26 +0200

    Add validation to cat

--- src/api/routes/cat-router.js
+++ src/api/routes/cat-router.js
@@ -11,20 +11,31 @@ import {
 
 // multer imports
 
-import { authenticateToken } from '../../middlewares/authentification.js';
-import { upload } from '../../middlewares/upload.js';
+import {authenticateToken} from '../../middlewares/authentification.js';
+import {upload} from '../../middlewares/upload.js';
+import {body} from 'express-validator';
+import {validationErrors} from '../../middlewares/error-handlers.js';
 
 const catRouter = express.Router();
 
 //console.log('Routerissa ollaan');
 
-catRouter.route('/').get(getCat).post(authenticateToken, upload.single('file'), postCat);
+catRouter
+  .route('/')
+  .get(getCat)
+  .post(
+    authenticateToken,
+    upload.single('file'),
+    body('cat_name').trim().isLength({min: 3, max: 128}).escape(),
+    body('weight').trim().isNumeric().toFloat(),
+    validationErrors,
+    postCat
+  );
 
 // omat kuvat
 catRouter.route('/user').get(authenticateToken, getMyCats);
 // jonkun muun käyttäjän kuvat
 catRouter.route('/user/:id').get(authenticateToken, getCatsByUserId);
 catRouter.route('/:id').get(getCatById).put(putCat).delete(deleteCat);
-
 
 export default catRouter;
