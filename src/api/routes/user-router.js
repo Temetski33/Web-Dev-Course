3324c260e3788df627687761c496ec0b660f4014
Temetski33 <teemu.laasio@gmail.com>
2025-12-12 02:49:23 +0200

    Add user post error handling

--- src/api/routes/user-router.js
+++ src/api/routes/user-router.js
@@ -7,12 +7,23 @@ import {
   deleteUser,
 } from '../controllers/user-controller.js';
 
+import {body} from 'express-validator';
+import {validationErrors} from '../../middlewares/error-handlers.js';
+
 const userRouter = express.Router();
 
 
 //console.log('Routerissa ollaan');
 
-userRouter.route('/').get(getUser).post(postUser);
+userRouter.route('/')
+   .get(getUser)
+   .post(
+     body('email').trim().isEmail(),
+     body('username').trim().isLength({min: 3, max: 20}).isAlphanumeric(),
+     body('password').trim().isLength({min: 8}),
+     validationErrors,
+     postUser
+   );
 
 userRouter.route('/:id').get(getUserById).put(putUser).delete(deleteUser);
 
